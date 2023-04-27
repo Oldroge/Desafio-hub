9bbeb57b00080f25a5534f696be94043f9c7fc62
Oldroge <rogeriomrjr@gmail.com>
2023-04-27 17:40:09 -0300

    feat: controller que faz o Post da nova pessoa

--- src/main/java/com/pure/desafio/controller/PessoaController.java
+++ src/main/java/com/pure/desafio/controller/PessoaController.java
@@ -2,7 +2,10 @@ package com.pure.desafio.controller;
 
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
@@ -26,6 +29,11 @@ public class PessoaController {
   public String findAll() {
     ArrayList<Pessoa> todasPessoas = this.pessoaService.findAll();
     return this.gson.toJson(todasPessoas);
+  }
+
+  @PostMapping
+  public ResponseEntity<Pessoa> addNewPessoa(@RequestBody Pessoa novaPessoa) {
+    return ResponseEntity.ok().body(pessoaService.addNewPessoa(novaPessoa));
   }
 
 }
