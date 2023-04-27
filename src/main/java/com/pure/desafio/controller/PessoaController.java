263276a9776db8b1bc200c765a12de196ed5c2bb
Oldroge <rogeriomrjr@gmail.com>
2023-04-27 00:09:47 -0300

    feat: adicionando método no controller que retorna o json

---
+++ src/main/java/com/pure/desafio/controller/PessoaController.java
@@ -0,0 +1,31 @@
+package com.pure.desafio.controller;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.pure.desafio.model.Pessoa;
+import com.pure.desafio.service.PessoaService;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+  @Autowired
+  PessoaService pessoaService;
+  Gson gson;
+
+  public PessoaController() {
+    this.pessoaService = new PessoaService();
+    this.gson = new Gson();
+  }
+
+  @GetMapping("/")
+  public String findAll() {
+    ArrayList<Pessoa> todasPessoas = this.pessoaService.findAll();
+    return this.gson.toJson(todasPessoas);
+  }
+
+}
